0a43d811e71fb29bded0d8fc13a1994c58705d5c
Tobias Kunze <r@rixx.de>
2019-03-15 21:14:53 +0100

    Use FallbackJSONField

--- src/when/event/models.py
+++ src/when/event/models.py
@@ -1,10 +1,12 @@
 import pytz
 from django.db import models
+from django.utils.translation import ugettext_lazy as _
+from jsonfallback.fields import FallbackJSONField
 
 
 class Event(models.Model):
     """
-    The event class is the central class in when.events. It contains all
+    The Event class is the central class in when.events. It contains all
     data related to an event, and is pretty much a direct mirror of the
     when.events API. Fields are influenced by the schema.org Event model,
     but extended and modified to fit our purposes.
@@ -21,31 +23,37 @@ class Event(models.Model):
         choices=[(tz, tz) for tz in pytz.common_timezones], max_length=30, default="UTC"
     )
 
-    organizer = models.CharField(max_length=200, null=True)
-    email = models.EmailField(null=True)
-    is_accessible_for_free = models.BooleanField(default=False)
-    languages = models.CharField(max_length=200, null=True)
-    maximum_attendee_capacity = models.PositiveIntegerField(null=True)
-    location = models.CharField(max_length=200, null=True)  # TODO: find better model
-    description = models.TextField(null=True)
-    color = models.CharField(max_length=6, null=True)
+    organizer = models.CharField(max_length=200, null=True, verbose_name=_("Organizer"))
+    email = models.EmailField(null=True, verbose_name=_("Email"))
+    is_accessible_for_free = models.BooleanField(
+        default=False, verbose_name=_("Is this event free?")
+    )
+    languages = models.CharField(max_length=200, null=True, verbose_name=_("Languages"))
+    maximum_attendee_capacity = models.PositiveIntegerField(
+        null=True, verbose_name=_("Maximum attendee capacity")
+    )
+    location = models.CharField(
+        max_length=200, null=True, verbose_name=_("Location")
+    )  # TODO: find better model
+    description = models.TextField(null=True, verbose_name=_("Description"))
+    color = models.CharField(max_length=6, null=True, verbose_name=_("Color"))
 
-    home_url = models.URLField(null=True)
-    ticket_url = models.URLField(null=True)
-    program_url = models.URLField(null=True)
-    cfp_url = models.URLField(null=True)
-    coc_url = models.URLField(null=True)
-    recording_url = models.URLField(null=True)
-    logo_url = models.URLField(null=True)
+    home_url = models.URLField(null=True, verbose_name=_("Home page"))
+    ticket_url = models.URLField(null=True, verbose_name=_("Ticket shop"))
+    program_url = models.URLField(null=True, verbose_name=_("Program"))
+    cfp_url = models.URLField(null=True, verbose_name=_("CfP"))
+    coc_url = models.URLField(null=True, verbose_name=_("CoC"))
+    recording_url = models.URLField(null=True, verbose_name=_("Recordings"))
+    logo_url = models.URLField(null=True, verbose_name=_("Logo URL"))
 
-    hashtag = models.CharField(max_length=50, null=True)
-    social_media_accounts = models.TextField(null=True)
-    tooling = models.TextField(null=True)
+    hashtag = models.CharField(max_length=50, null=True, verbose_name=_("Hashtag"))
+    social_media_accounts = FallbackJSONField(null=True)
+    tooling = FallbackJSONField(null=True)
 
     tags = models.TextField(null=True)  # Contains topics, locations, …
 
     last_updated = models.DateTimeField(null=True)
-    last_response = models.TextField(null=True)
+    last_response = FallbackJSONField(null=True)
     state = models.CharField(
         choices=(
             ("new", "new"),

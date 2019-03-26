eb0a8f99f641d8bc1a1b5b2812e7533c861767fc
Tobias Kunze <r@rixx.de>
2019-03-26 23:57:34 +0100

    Random os methods are not good enough

--- src/when/schema/__init__.py
+++ src/when/schema/__init__.py
@@ -5,5 +5,5 @@ VERSIONS = ['0.1.0']
 
 
 def get_schema(version):
-    with open(os.path.join(os.path.basename(__file__), 'schema-' + str(version) + '.json')) as f:
+    with open(os.path.join(os.path.dirname(__file__), 'schema-' + str(version) + '.json')) as f:
         return json.load(f)

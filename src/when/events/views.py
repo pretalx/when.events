5514b0387ad68b1bf65a919e889ec97716828853
Tobias Kunze <r@rixx.de>
2019-03-30 17:19:19 +0100

    Add a validator

--- src/when/events/views.py
+++ src/when/events/views.py
@@ -1,9 +1,12 @@
 import json
 import os
 
+from django.contrib import messages
+from django.utils.translation import ugettext_lazy as _
 from django.views.generic import TemplateView
+from jsonschema import validate
 
-from when.schema import get_schema
+from when import schema
 
 
 class Docs(TemplateView):
@@ -11,7 +14,7 @@ class Docs(TemplateView):
 
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
-        context['schema'] = get_schema('0.1.0')
+        context['schema'] = schema.get_schema('0.1.0')
         context['current_example'] = json.dumps(json.load(
             open(os.path.join(
                 os.path.dirname(os.path.dirname(os.path.dirname(__file__))),
@@ -19,6 +22,28 @@ class Docs(TemplateView):
             ))
         ), indent=4)
         return context
+
+
+class Validator(TemplateView):
+    template_name = 'events/validator.html'
+
+    def post(self, request):
+        try:
+            data = json.loads(request.POST.get('data'))
+            version = data.get('version')
+        except Exception as e:
+            messages.error(request, _('Could not parse data: ') + str(e))
+            return super().get(request)
+        if not version or version not in schema.VERSIONS:
+            messages.error(request, _('Incorrect schema version supplied. Supported versions are: ') + ', '.join(schema.VERSIONS))
+            return super().get(request)
+        used_schema = schema.get_schema(data.get("version"))
+        try:
+            validate(data, used_schema)
+            messages.success(request, _('Looking good!'))
+        except Exception as e:
+            messages.error(request, _('Invalid data: ') + e.message)
+        return super().get(request)
 
 
 class LogList(TemplateView):
--- src/when/urls.py
+++ src/when/urls.py
@@ -6,6 +6,7 @@ from .events import views
 
 urlpatterns = [
     url('^docs$', views.Docs.as_view(), name='docs'),
+    url('^docs/validator$', views.Validator.as_view(), name='docs'),
     url('^log$', views.LogList.as_view(), name='logs'),
     url('^events$', views.EventList.as_view(), name='events'),
     # url('^events/calendar$', views.EventCalendar.as_view(), name='events'),
